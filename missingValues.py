eb12f58774a9032816b1f2500acbc0e535ae3a70
sirichandanagangam <97136285+sirichandanagangam@users.noreply.github.com>
2022-02-27 12:04:46 -0800

    Update missingValues.py

--- missingValues.py
+++ missingValues.py
@@ -2,7 +2,6 @@ class Solution(object):
     def missingNumber(self, nums):
         n=len(nums)
         for i in range(0,n+1):
-            for j in range(0,n):
                 if i not in nums:
                     return i
         
